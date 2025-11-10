9792af92b89d85a8d4b8e1264827424eb0f6f22f
Théo Matrod <75441296+C4RBON73@users.noreply.github.com>
2025-11-10 11:43:00 +0100

    Loading
    problème dans le load (probablement un problème d'index)

--- Assets/Loader.cs
+++ Assets/Loader.cs
@@ -3,6 +3,7 @@ using System.IO;
 using System.Linq;
 using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 
 public class Loader : MonoBehaviour
@@ -15,25 +16,14 @@ public class Loader : MonoBehaviour
     private int nbSides;
     private int nbRidges;
     private Vector3[] vertexs;
-    private Sides[] sides;
+    private int[] triangles;
+    private Vector3 CoG;
+    
+    //La coordonné la plus éloigner du centre de gravité
+    private Vector3 normalReference = new Vector3(0f,0f,0f); 
 
    
-    public class Sides
-        {
-            public int size;
-            public int[] list;
 
-            public Sides(int size)
-            {
-                this.size = size;
-                list = new int[size];
-            }
-
-            public void addList(int[] list)
-            {
-                this.list = list;
-            }
-    }
 
     public void loadStructure(string path)
     {
@@ -54,32 +44,45 @@ public class Loader : MonoBehaviour
         nbSides = int.Parse(split_line[1]);
         nbRidges = int.Parse(split_line[2]);
         vertexs = new Vector3[nbVertex];
-        sides = new Sides[nbSides];
+        triangles = new int[nbSides *3];
+        
 
         //Initialisation de vertexs
         for (int i = 0; i < nbVertex; i++) {
             line = reader.ReadLine();
             split_line = line.Split(" ");
-            vertexs.Append(new Vector3(float.Parse(split_line[0],CultureInfo.InvariantCulture), float.Parse(split_line[1], CultureInfo.InvariantCulture), float.Parse(split_line[2], CultureInfo.InvariantCulture)));
+            vertexs[i] =(new Vector3(float.Parse(split_line[0],CultureInfo.InvariantCulture), float.Parse(split_line[1], CultureInfo.InvariantCulture), float.Parse(split_line[2], CultureInfo.InvariantCulture)));
+            normalReference.x = Mathf.Max(normalReference.x,(vertexs[i].x));
+            normalReference.y = Mathf.Max(normalReference.y, (vertexs[i].y));
+            normalReference.z = Mathf.Max(normalReference.z, (vertexs[i].z));
 
+            //Pour ne pas repasser dedans lors du calcul de CoG
+            CoG += vertexs[i];
         }
-        Debug.Log(nbVertex);
-        Debug.Log(nbSides); 
+        //Calcul final de CoG
+        CoG = CoG / nbVertex;
+
+
         //Initialisation de sides
         for (int y = 0; y < nbSides; y++) {
             line = reader.ReadLine();
             split_line = line.Split(" ");
 
-            //Nécessaire afin de convertir les strings du fichiers
-            Sides temp = new Sides(int.Parse(split_line[0]));
-            int[] temp2 = new int[int.Parse(split_line[0])];
-            
+
             //Pour récupérer les valeurs et les convertir en Int dans une liste, on Skip 1 car c'est la taille des sides et pas une valeur
-            for (var z =0; z < int.Parse(split_line[0]); z++) {
-                temp2.Append(int.Parse(split_line[z]));
+            for (var z= 1; z <= int.Parse(split_line[0]); z++) {
+                
+                triangles[y + z-1] = (int.Parse(split_line[z]));
             }
-            temp.addList(temp2);
-            sides.Append(temp);
+        }
+
+        Debug.Log(normalReference);
+        if (normalReference.x > 0 && normalReference.y > 0 && normalReference.z > 0)
+        {
+            for (int u = 0; u < vertexs.Length; u++)
+            {
+                //vertexs[u] = new Vector3(vertexs[u].x / normalReference.x, vertexs[u].y / normalReference.y, vertexs[u].z / normalReference.z);
+            }
         }
 
 
@@ -92,15 +95,7 @@ public class Loader : MonoBehaviour
 
          
 
-        int[] triangles = new int[nbSides * 3];
 
-        int t = 0;
-        for (int i = 0; i < nbSides; i++) {
-            triangles[t] = sides[i].list[0];
-            triangles[t + 1] = sides[i].list[1];
-            triangles[t + 2] = sides[i].list[2];
-            t += 3;
-        }
 
         mesh.vertices = vertexs;
         mesh.triangles = triangles;
